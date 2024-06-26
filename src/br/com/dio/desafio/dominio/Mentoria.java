3a2916555389235e5e70c1382372b53ad3a67ca2
lucasalves0722 <lucasalves098messias@gmail.com>
2024-06-25 22:40:49 -0300

    mentoria e curso

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,49 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    //Atributos:
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    //Método construtor:
+    public Mentoria() {
+    }
+
+    //Getter and Setters:
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    //toString:
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
